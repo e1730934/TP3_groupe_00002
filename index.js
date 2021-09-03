ca3b1d7c6a63c05c60d273f2dafc98f06311a420
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2021-09-03 10:16:19 -0400

    index.js + update package.json

---
+++ index.js
@@ -0,0 +1,41 @@
+function Multiplier (nombre1, nombre2) {
+    return nombre1 * nombre2
+}
+
+function Diviser (nombre1, nombre2){
+    if (nombre1 === 0 || nombre2 === 0)
+        return  NaN
+    else
+        return nombre1/nombre2
+}
+
+function SommeDesElementsDuTableau(tableau){
+    if (tableau === undefined || tableau ===null)
+        return NaN
+    else if (tableau.length === 0)
+        return 0
+    else {
+        let somme = 0;
+        for (let i = 0; i < tableau.length; i++) {
+            somme += tableau[i]
+        }
+        return somme
+    }
+}
+
+function ListeElementsMultiplesDe2(tableau){
+    let list = []
+    if (tableau === undefined || tableau ===null)
+        return NaN
+    else {
+        for (let i = 0; i < tableau.length; i++) {
+            if (tableau[i] % 2 === 0) {
+                list.push(tableau[i])
+            }
+        }
+        return list
+    }
+}
+
+
+console.log(ListeElementsMultiplesDe2([1,2,3,4]))
